aa1453ce18d7f893506eda988d4d144c48a42af5
akshaygohel39004 <akshay.gohel@pirimidtech.com>
2026-01-21 12:24:59 +0530

    feat(Entity):create user and role entity for security purpose.

---
+++ websockettask/src/main/java/com/akshay/websockettask/entity/AuthProvider.java
@@ -0,0 +1,7 @@
+package com.akshay.websockettask.entity;
+
+public enum AuthProvider {
+    LOCAL,      // form login, basic auth
+    GOOGLE,     // google oauth2
+    GITHUB      // github oauth2
+}
---
+++ websockettask/src/main/java/com/akshay/websockettask/entity/RefreshToken.java
@@ -0,0 +1,35 @@
+package com.akshay.websockettask.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 500)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
