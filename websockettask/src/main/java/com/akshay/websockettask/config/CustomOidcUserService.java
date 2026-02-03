1dd5ab04370ab8bfac5eb954c87095127a09f4ae
akshaygohel39004 <akshay.gohel@pirimidtech.com>
2026-02-03 10:56:10 +0530

    fix(auth): add roles to Google OAuth principal.

--- websockettask/src/main/java/com/akshay/websockettask/config/CustomOidcUserService.java
+++ websockettask/src/main/java/com/akshay/websockettask/config/CustomOidcUserService.java
@@ -1,11 +1,18 @@
 package com.akshay.websockettask.config;
 
+import com.akshay.websockettask.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,12 +26,26 @@ public class CustomOidcUserService
             throws OAuth2AuthenticationException {
 
         OidcUser oidcUser = super.loadUser(request);
-        processor.processUser(
+        User user=processor.processUser(
                 request.getClientRegistration()
                         .getRegistrationId(),
                 oidcUser
         );
 
-        return oidcUser;
+        // Map roles
+        Set<GrantedAuthority> authorities =
+                user.getRoles().stream()
+                        .map(r -> new SimpleGrantedAuthority(
+                                r.getType().name()))
+                        .collect(Collectors.toSet());
+
+        // Keep default authorities (SCOPE_*)
+        authorities.addAll(oidcUser.getAuthorities());
+
+        return new DefaultOidcUser(
+                authorities,
+                oidcUser.getIdToken(),
+                oidcUser.getUserInfo()
+        );
     }
 }
